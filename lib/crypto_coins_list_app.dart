8f89a4e6405d5d9bc8e939ec39880ad3e24f67a7
Kamran Aghayev <aghayev.kamran@outlook.com>
2023-12-13 12:54:32 +0400

    autoroute added

--- lib/crypto_coins_list_app.dart
+++ lib/crypto_coins_list_app.dart
@@ -1,21 +1,29 @@
-import 'package:flutter/material.dart';
 import 'package:crypto_coins_list/router/router.dart';
+import 'package:flutter/material.dart';
 import 'package:crypto_coins_list/theme/theme.dart';
 import 'package:get_it/get_it.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
-class CryptoCurrenciesListApp extends StatelessWidget {
+class CryptoCurrenciesListApp extends StatefulWidget {
   const CryptoCurrenciesListApp({super.key});
 
   @override
+  State<CryptoCurrenciesListApp> createState() =>
+      _CryptoCurrenciesListAppState();
+}
+
+class _CryptoCurrenciesListAppState extends State<CryptoCurrenciesListApp> {
+  final _appRouter = AppRouter();
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'CryptoCurrenciesList',
       theme: darkTheme,
-      routes: routes,
-      navigatorObservers: [
-        TalkerRouteObserver(GetIt.I<Talker>()),
-      ],
+      routerConfig: _appRouter.config(
+        navigatorObservers: () => [
+          TalkerRouteObserver(GetIt.I<Talker>()),
+        ],
+      ),
     );
   }
 }
